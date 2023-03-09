b8f078c933f4ed48f54100ee7c6123d8e6d22554
NicoleMGimenez <magagimenez03@gmail.com>
2023-03-09 15:10:54 -0300

    Conexiones a la tabla

--- src/app/components/datatable/datatable.component.ts
+++ src/app/components/datatable/datatable.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Reserva } from 'src/app/models/reserva';
 import { ReservaService } from 'src/app/services/reserva.service';
+import { SingleCheckComponent } from '../single-check/single-check.component';
 
 @Component({
   selector: 'app-datatable',
@@ -12,46 +14,55 @@ import { ReservaService } from 'src/app/services/reserva.service';
 })
 export class DatatableComponent implements OnInit {
 
-  displayedColumns: string[] = ['DNI', 'Llegada', 'Salida', 'NomApe', "acciones"];
+  displayedColumns: string[] = ['Titular', 'NomApe', 'fechaLlegada', 'fechaSalida', "acciones"];
   dataSource = new MatTableDataSource<Reserva>([]);
 
   reservas: Array<Reserva> = [];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  searchKey!: string;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
-  
-  constructor(private reservaService:ReservaService,
-    private router: Router) {
-      this.obtenerReservas();
-     }
+
+  constructor(private reservaService: ReservaService,
+    private router: Router, private modalService: NgbModal) {
+
+  }
 
   ngOnInit(): void {
-    this.dataSource.data = this.reservas;
+    this.obtenerReservas();
+    //  console.log(this.reservas, "reservas");
+    //  this.dataSource.data = this.reservas;
+    //  console.log(this.dataSource.data, "data");
+
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+    
+    console.log(this.dataSource.data, "filter")
+    // if (this.dataSource.paginator) {
+    //   this.dataSource.paginator.firstPage();
+    // }
   }
 
-   getRecord()
-  {
-    alert("Pasa la info");
-  }
 
-  obtenerReservas() {
+  open() {
+		const modalRef = this.modalService.open(SingleCheckComponent);
+		modalRef.componentInstance.name = 'World';
+	}
+
+  async obtenerReservas() {
+    await new Promise(f => setTimeout(f, 10));
     this.reservaService.getReservas().subscribe(
       (result) => {
-        console.log(result);
-        this.reservas = result;
-        console.log(this.reservas, 'pasajes cargados')
+        this.reservas = result.msg;
+        this.dataSource.data = this.reservas;
+        console.log(this.dataSource.data, "data");
       },
       error => { alert("Error en la petición"); }
     )
